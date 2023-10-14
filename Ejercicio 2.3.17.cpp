9947e81930a32ee2472cfd6dbbc90eaf92b4850c
Valerio Gomez <egomeza@est.unap.edu.pe>
2023-10-14 10:29:02 -0500

    2.3.17

--- Ejercicio 2.3.17.cpp
+++ Ejercicio 2.3.17.cpp
@@ -6,22 +6,22 @@ using namespace std;
 
 int main() {
     map<string, int> indexMap;
-    int currentIndex = 0;  // Remove the extra '.' at the end of this line.
-    string cities[] = {"Puno", "Ilave", "Juliaca", "Juli", "Acora"};
-    int numCities = sizeof(cities) / sizeof(cities[0]);
-    for (int i = 0; i < numCities; i++) {
-        const string& city = cities[i];
+    int contador = 0;  
+    string ciudades[] = {"Puno", "Ilave", "Juliaca", "Juli", "Acora"};
+    int numciudades = sizeof(ciudades) / sizeof(ciudades[0]);
+    for (int i = 0; i < numciudades; i++) {
+        const string& ciudad = ciudades[i];
         // Verificamos si la ciudad ya está en el mapa.
-        if (indexMap.find(city) == indexMap.end()) {
-            // Si no está, le asignamos un nuevo �ndice y la almacenamos en el mapa.
-            indexMap[city] = currentIndex++;
+        if (indexMap.find(ciudad) == indexMap.end()) {
+            // Si no está, le asignamos un nuevo Indice y la almacenamos en el mapa.
+            indexMap[ciudad] = contador++;
         }
     }
     // Ahora puedes acceder al Indice de cada ciudad de forma eficiente.
-    for (int i = 0; i < numCities; i++) {
-        const string& city = cities[i];
-        int index = indexMap[city];
-        cout << city << " tiene el Indice " << index << endl;
+    for (int i = 0; i < numciudades; i++) {
+        const string& ciudad = ciudades[i];
+        int index = indexMap[ciudad];
+        cout << ciudad << " tiene el Indice " << index << endl;
     }
     return 0;
 }
