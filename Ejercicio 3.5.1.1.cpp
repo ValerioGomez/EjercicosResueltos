f0801ad9807905e8ff0887fe1747ba7a2decb478
Valerio Gomez <egomeza@est.unap.edu.pe>
2023-10-14 22:21:57 -0500

    Ejercicio 3.5.1.2

--- Ejercicio 3.5.1.1.cpp
+++ Ejercicio 3.5.1.1.cpp
@@ -1,6 +1,4 @@
-//Ejercicio 3.5.1.1 
-
-#include <cstdio>
+#include <iostream>
 #include <cstring>
 using namespace std;
 
@@ -13,36 +11,45 @@ int main() {
   C = 3;  // Establece C en 3 para el caso de prueba proporcionado
 
   // Precios de las 3 prendas
-  precio[0][0] = 3;
-  precio[0][1] = 6;
-  precio[0][2] = 4;
-  precio[0][3] = 8;
+  precio[0][0] = 6;
+  precio[0][1] = 4;
+  precio[0][2] = 8;
 
-  precio[1][0] = 2;
-  precio[1][1] = 10;
-  precio[1][2] = 6;
+  precio[1][0] = 10;
+  precio[1][1] = 6;
 
-  precio[2][0] = 4;
-  precio[2][1] = 7;
-  precio[2][2] = 3;
-  precio[2][3] = 1;
-  precio[2][4] = 5;
+  precio[2][0] = 7;
+  precio[2][1] = 3;
+  precio[2][2] = 1;
+  precio[2][3] = 5;
 
   memset(alcanzable, false, sizeof alcanzable);         
 
-  for (i = 1; i <= precio[0][0]; i++)       
-    if (M - precio[0][i] >= 0)      
-      alcanzable[0][M - precio[0][i]] = true;  
+  for (i = 1; i <= precio[0][0]; i++) {
+    if (M - precio[0][i] >= 0) {
+      alcanzable[0][M - precio[0][i]] = true;
+    }
+  }
 
-  for (i = 1; i < C; i++)                   
-    for (j = 0; j < M; j++) if (alcanzable[i - 1][j]) 
-      for (k = 1; k <= precio[i][0]; k++) if (j - precio[i][k] >= 0)
-        alcanzable[i][j - precio[i][k]] = true;   
+  for (i = 1; i < C; i++) {
+    for (j = 0; j < M; j++) {
+      if (alcanzable[i - 1][j]) {
+        for (k = 1; k <= precio[i][0]; k++) {
+          if (j - precio[i][k] >= 0) {
+            alcanzable[i][j - precio[i][k]] = true;
+          }
+        }
+      }
+    }
+  }
 
   for (j = 0; j <= M && !alcanzable[C - 1][j]; j++);
-
-  if (j == M + 1) printf("no hay solución\n");         
-  else            printf("%d\n", M - j);
+  
+  if (j == M + 1) {
+    cout << "La Salida es no hay solución" << endl;
+  } else {
+    cout << " " << M - j << endl;
+  }
 
   return 0;
 }
